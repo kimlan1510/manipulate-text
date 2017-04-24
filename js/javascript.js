f2bb5630cccb35e1cf40f8055e5625d20689bbf4
buna ezeofor and kimlan nguyen <buna@>
2017-04-24 16:38:15 -0700

    add click bait

--- js/javascript.js
+++ js/javascript.js
@@ -1,30 +1,41 @@
-var sentence = prompt("Type your sentence");
-var new_sentence = function(sentence){
-  return sentence.charAt(0).toUpperCase() +
-  sentence.charAt(sentence.length-1).toUpperCase();
-};
-function reverse(letter){
-  var split_letter = letter.split("");
-  var reverseArray = split_letter.reverse();
-  var joinArray = split_letter.join("");
-  return joinArray;
-};
-function join(sentence){
-  return sentence.concat(reverse(new_sentence(sentence)));
-};
-function final_sentence(sentence){
-  return sentence.charAt((sentence.length-sentence.length%2)/2) + join(sentence);
-};
+$(document).ready(function(){
+  var sentence = prompt("Type your sentence");
+  var new_sentence = function(sentence){
+    return sentence.charAt(0).toUpperCase() +
+    sentence.charAt(sentence.length-1).toUpperCase();
+  };
+  function reverse(letter){
+    var split_letter = letter.split("");
+    var reverseArray = split_letter.reverse();
+    var joinArray = reverseArray.join("");
+    return joinArray;
+  };
+  function join(sentence){
+    return sentence.concat(reverse(new_sentence(sentence)));
+  };
+  function final_sentence(sentence){
+    return sentence.charAt((sentence.length-sentence.length%2)/2) + join(sentence);
+  };
 
-function reverse_final(sentence){
-  var split_string = sentence.split("")
-  var reverseArray = split_string.reverse();
-  var joinArray = reverseArray.join("");
-  return joinArray;
-};
-var modifiedSentence = reverse_final(final_sentence(sentence));
+  function reverse_final(sentence){
+    var split_string = sentence.split("")
+    var reverseArray = split_string.reverse();
+    var joinArray = reverseArray.join("");
+    return joinArray;
+  };
+  var modifiedSentence = reverse_final(final_sentence(sentence));
 
-/*alert(reverse(new_sentence(sentence)));
-alert(join(sentence));
-alert(final_sentence(sentence));
-alert(reverse_final(final_sentence(sentence)));*/
+  alert(reverse(new_sentence(sentence)));
+  alert(join(sentence));
+  alert(final_sentence(sentence));
+  alert(reverse_final(final_sentence(sentence)));
+
+  $("#dog").click(function() {
+    alert(sentence);
+  });
+
+  $("#cat").click(function() {
+    alert(reverse_final(final_sentence(sentence)));
+  });
+
+});
